b836c70ef7d80c360893815f8a8b7df6432cf51e
escobedojoseeduardo007-cell <escobedojoseeduardo007@gmail.com>
2026-09-17 22:41:30 -0700

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,25 @@
+public class Main{
+    static void selection(int[] a){
+        for(int i=0;i<a.length;i++){
+            int small=i;
+            for(int j=i+1;j<a.length;j++){
+                if(a[small]>a[j])
+                    small=j;
+            }
+            int temp=a[i];
+            a[i]=a[small];
+            a[small]=temp;
+        }
+    }
+    static void printArr(int[] a){
+        for(int i=0;i<a.length;i++)System.out.print(a[i]+" ");
+    }
+    public static void main(String[] args){
+        int[] a={65,26,13,23,12};
+        System.out.println("Arreglo antes de ser ordenado:");
+        printArr(a);
+        selection(a);
+        System.out.println("\nArreglo despues de ser ordenado");
+        printArr(a);
+    }
+}
